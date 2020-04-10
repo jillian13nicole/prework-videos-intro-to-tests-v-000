93ac1bbca1e4bce6cec4d0255525ef5d9d115497
Jillian Weimer <jilliannweimer@gmail.com>
2020-04-10 15:16:52 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../conversions.rb'
 
-desbribe "conversions" do
+describe "conversions" do
   describe "#ounces_to_grams" do
     it "given 0, returns 0.0"
       grams = ounces_to_grams(0)
